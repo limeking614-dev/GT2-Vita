5de087c2aa68f912c6ea4ac3c4c89a07b18edccd
limeking614-dev <limeking614@gmail.com>
2026-09-04 12:22:03 -0500

    Add initial GT2 runtime
    Implement basic runtime functions for GT2 integration.

---
+++ runtime/gt2_runtime.c
@@ -0,0 +1,25 @@
+#include "gt2_api.h"
+
+int gt2_runtime_init(void)
+{
+    /*
+     * Runtime initialization will eventually connect
+     * the decompiled GT2 game to the Vita platform layer.
+     */
+
+    return 0;
+}
+
+void gt2_runtime_frame(void)
+{
+    /*
+     * GT2 game loop will be connected here.
+     */
+}
+
+void gt2_runtime_shutdown(void)
+{
+    /*
+     * Runtime shutdown will be connected here.
+     */
+}
