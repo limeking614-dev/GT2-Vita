7415d46aa2e6ce4670c3edee0702beabbac89059
limeking614-dev <limeking614@gmail.com>
2026-09-04 12:17:42 -0500

    Add Vita platform implementation
    Added initial implementation for Vita platform initialization and shutdown functions.

---
+++ vita/gt2_vita_platform.c
@@ -0,0 +1,24 @@
+#include "gt2_vita_platform.h"
+
+int gt2_vita_platform_init(void)
+{
+    /*
+     * Vita initialization will be added here.
+     *
+     * Future systems:
+     * - filesystem
+     * - controller
+     * - graphics
+     * - audio
+     * - timing
+     */
+
+    return 0;
+}
+
+void gt2_vita_platform_shutdown(void)
+{
+    /*
+     * Vita shutdown will be added here.
+     */
+}
