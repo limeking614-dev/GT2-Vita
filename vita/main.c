7272acb2979d885bcee6413b5cc448c9dbf710fa
limeking614-dev <limeking614@gmail.com>
2026-09-04 12:18:46 -0500

    Connect Vita entry point to platform layer

--- vita/main.c
+++ vita/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+#include "gt2_vita_platform.h"
+
 int main(void)
 {
     printf("GT2-Vita starting...\n");
+
+    if (gt2_vita_platform_init() != 0)
+    {
+        printf("GT2-Vita platform initialization failed.\n");
+        return 1;
+    }
+
+    printf("GT2-Vita platform initialized.\n");
+
+    gt2_vita_platform_shutdown();
 
     return 0;
 }
